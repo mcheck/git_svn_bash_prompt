0fd0e9fc7adaff76428e62a6439109180c5661c0
Scott Woods <scott@westarete.com>
2009-08-15 11:00:40 -0400

    Add support for subversion repository tags/branches.

--- git_svn_bash_prompt.sh
+++ git_svn_bash_prompt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #
-# Set our bash prompt according to the branch/status of the current git 
-# repository.
+# Set our bash prompt according to:
+#  * the branch/status of the current git repository
+#  * the branch of the current subversion repository
+#  * the return value of the previous command
+# 
+# Originally based on http://gist.github.com/31934
 #
-# Forked from http://gist.github.com/31934
+# Scott Woods <scott@westarete.com>
+# West Arete Computing
 
+# The various escape codes that we can use to color our prompt.
         RED="\[\033[0;31m\]"
      YELLOW="\[\033[0;33m\]"
       GREEN="\[\033[0;32m\]"
@@ -15,14 +21,18 @@ LIGHT_GREEN="\[\033[1;32m\]"
  LIGHT_GRAY="\[\033[0;37m\]"
  COLOR_NONE="\[\e[0m\]"
 
+# Detect whether the current directory is a git repository.
 function is_git_repository {
   git branch > /dev/null 2>&1
 }
 
+# Detect whether the current directory is a subversion repository.
+function is_svn_repository {
+  test -d .svn
+}
+
+# Determine the branch/state information for this git repository.
 function parse_git_branch {
-  # Only display git info if we're inside a git repository.
-  is_git_repository || return 1
-  
   # Capture the output of the "git status" command.
   git_status="$(git status 2> /dev/null)"
 
@@ -55,23 +65,49 @@ function parse_git_branch {
     branch=${BASH_REMATCH[1]}
   fi
 
-  # Display the prompt.
+  # Return the prompt.
   echo "${state}(${branch})${remote}${COLOR_NONE} "
 }
 
+# Determine the branch information for this subversion repository.
+function parse_svn_branch {
+  # Capture the output of the "git status" command.
+  svn_info="$(svn info | egrep '^URL: ' 2> /dev/null)"
+
+  # Get the name of the branch.
+  branch_pattern="^URL: .*/(branches|tags)/([^/]+)"
+  trunk_pattern="^URL: .*/trunk(/.*)?$"
+  if [[ ${svn_info} =~ $branch_pattern ]]; then
+    branch=${BASH_REMATCH[2]}
+  elif [[ ${svn_info} =~ $trunk_pattern ]]; then
+    branch='trunk'
+  fi
+
+  # Return the prompt.
+  echo "(${branch}) "
+}
+
+# Return the prompt symbol to use, colorized based on the return value of the
+# previous command.
 function prompt_symbol () {
-  # Set color of dollar prompt based on return value of previous command.
-  if test $1 -eq 0
-  then
+  if test $1 -eq 0 ; then
       echo "\$"
   else
       echo "${RED}\$${COLOR_NONE}"
   fi
 }
 
+# Set the full prompt.
 function prompt_func () {
   last_return_value=$?
-  PS1="\u@\h \w $(parse_git_branch)$(prompt_symbol $last_return_value) "
+  if is_git_repository ; then
+    branch="$(parse_git_branch)"
+  elif is_svn_repository ; then
+    branch="$(parse_svn_branch)"
+  else
+    branch=''
+  fi
+  PS1="\u@\h \w $branch$(prompt_symbol $last_return_value) "
 }
 
 PROMPT_COMMAND=prompt_func
